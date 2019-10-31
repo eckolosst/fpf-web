f0b5e0dcfd14827c502d65f12928d61bd2f80cac
eckolosst <lucasp.1606@gmail.com>
2019-10-31 12:54:50 -0300

    Home terminada

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SearchesComponent } from './searches/searches.component';
 import { AdoptionsComponent } from './adoptions/adoptions.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [HomeComponent, SearchesComponent, AdoptionsComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, FlexLayoutModule, MatButtonModule]
 })
 export class ViewsModule {}
