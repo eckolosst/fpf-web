86ca5a60d3ecc9f91009acf38ae16bb2660ea02d
eckolosst <lucasp.1606@gmail.com>
2019-11-01 11:15:04 -0300

    vista de busqueda

--- src/app/views/searches/searches.component.ts
+++ src/app/views/searches/searches.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./searches.component.scss']
 })
 export class SearchesComponent implements OnInit {
+  public lat: number;
+  public lng: number;
+  public aIcon = 'assets/images/avistamiento-icon.png';
+  public pIcon = 'assets/images/perdido-icon.png';
 
-  constructor() { }
+  public petType;
+  public distances;
 
-  ngOnInit() {
+  constructor() {
+    /* Debería recuperar las coordenadas del dispositivo */
+    this.lat = -39.0687096;
+    this.lng = -67.6409601;
+
+    this.petType = ['Perro', 'Gato', 'Tortuga', 'Conejo', 'Cabra', 'Péz', 'Canguro', 'Jirafa'];
+    this.distances = ['1 km', '2 km', '3 km', '4 km', '5 km', '6 km', '7 km', '8 km', '9 km'];
   }
 
+  ngOnInit() {}
 }
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -4,10 +4,24 @@ import { HomeComponent } from './home/home.component';
 import { SearchesComponent } from './searches/searches.component';
 import { AdoptionsComponent } from './adoptions/adoptions.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [HomeComponent, SearchesComponent, AdoptionsComponent],
-  imports: [CommonModule, FlexLayoutModule, MatButtonModule]
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB5tJUUev07_4R3XvpL9x5s201tVGvCCrU'
+    })
+  ]
 })
 export class ViewsModule {}
