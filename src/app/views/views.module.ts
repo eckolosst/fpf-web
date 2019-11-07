c2d33b335120886509a032d6e65c753a26215b48
eckolosst <lucasp.1606@gmail.com>
2019-11-07 10:05:22 -0300

    probando última vista

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -4,7 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { SearchesComponent } from './searches/searches.component';
 import { AdoptionsComponent } from './adoptions/adoptions.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatIconModule,
+  MatRipple,
+  MatRippleModule
+} from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 
@@ -17,6 +25,8 @@ import { AgmCoreModule } from '@agm/core';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatIconModule,
+    MatRippleModule,
     FormsModule,
     ReactiveFormsModule,
     AgmCoreModule.forRoot({
